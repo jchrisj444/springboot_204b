2d295998c30c9f29e248083e18656ffb09ab2cfd
jchrisj <jchrisj444@verizon.net>
2019-04-13 19:31:57 -0400

    Initial commit

---
+++ src/main/java/com/jchrisj444/springboot_204b/Springboot204bApplication.java
@@ -0,0 +1,13 @@
+package com.jchrisj444.springboot_204b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot204bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot204bApplication.class, args);
+    }
+
+}
